25e088e691b0f558b4534bfa8d80e2a3c1d74c82
okazunori <okazunori4dead@gmail.com>
2026-04-04 19:37:38 +0900

    Added plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
@@ -9,82 +9,119 @@ return {
 
   -- == Examples of Adding Plugins ==
 
-  "andweeb/presence.nvim",
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
+  --
+  -- -- == Examples of Overriding Plugins ==
+  --
+  -- -- customize dashboard options
+  -- {
+  --   "folke/snacks.nvim",
+  --   opts = {
+  --     dashboard = {
+  --       preset = {
+  --         header = table.concat({
+  --           " █████  ███████ ████████ ██████   ██████ ",
+  --           "██   ██ ██         ██    ██   ██ ██    ██",
+  --           "███████ ███████    ██    ██████  ██    ██",
+  --           "██   ██      ██    ██    ██   ██ ██    ██",
+  --           "██   ██ ███████    ██    ██   ██  ██████ ",
+  --           "",
+  --           "███    ██ ██    ██ ██ ███    ███",
+  --           "████   ██ ██    ██ ██ ████  ████",
+  --           "██ ██  ██ ██    ██ ██ ██ ████ ██",
+  --           "██  ██ ██  ██  ██  ██ ██  ██  ██",
+  --           "██   ████   ████   ██ ██      ██",
+  --         }, "\n"),
+  --       },
+  --     },
+  --   },
+  -- },
+  --
+  -- -- You can disable default plugins as follows:
+  -- { "max397574/better-escape.nvim", enabled = false },
+  --
+  -- -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   config = function(plugin, opts)
+  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
+  --     local luasnip = require "luasnip"
+  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+  --
+  --     -- include the default astronvim config that calls the setup call
+  --     require "astronvim.plugins.configs.luasnip"(plugin, opts)
+  --   end,
+  -- },
+  --
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   config = function(plugin, opts)
+  --     require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+  --     -- add more custom autopairs configuration such as custom rules
+  --     local npairs = require "nvim-autopairs"
+  --     local Rule = require "nvim-autopairs.rule"
+  --     local cond = require "nvim-autopairs.conds"
+  --     npairs.add_rules(
+  --       {
+  --         Rule("$", "$", { "tex", "latex" })
+  --           -- don't add a pair if the next character is %
+  --           :with_pair(cond.not_after_regex "%%")
+  --           -- don't add a pair if  the previous character is xxx
+  --           :with_pair(
+  --             cond.not_before_regex("xxx", 3)
+  --           )
+  --           -- don't move right when repeat character
+  --           :with_move(cond.none())
+  --           -- don't delete if the next character is xx
+  --           :with_del(cond.not_after_regex "xx")
+  --           -- disable adding a newline when you press <cr>
+  --           :with_cr(cond.none()),
+  --       },
+  --       -- disable for .vim files, but it work for another filetypes
+  --       Rule("a", "a", "-vim")
+  --     )
+  --   end,
+  -- },
+
   {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
-
-  -- customize dashboard options
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
-          }, "\n"),
-        },
-      },
-    },
-  },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-
-      -- include the default astronvim config that calls the setup call
-      require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    -- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+    config = function()
+      require("nvim-surround").setup {
+        -- Put your configuration here
+      }
     end,
   },
 
   {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
+    "numToStr/Comment.nvim",
+    enabled = true, -- 標準機能より強力なので有効化
+    keys = {
+      {
+        "<C-/>",
+        function() require("Comment.api").toggle.linewise.current() end,
+        desc = "Comment toggle current line",
+      },
+      {
+        "<C-/>",
+        -- <ESC>してから呼び出すことで、選択範囲を正確に判定させます
+        "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+        mode = "v",
+        desc = "Comment toggle selection",
+      },
+    },
+    config = function()
+      require("Comment").setup {
+        -- ここを有効にすると、`v` (小文字) で選択した時に「範囲内だけ」をコメントにします
+        sticky = true,
+        mappings = { basic = true, extra = false },
+      }
     end,
   },
 }
